73ba98349c1a8d0660cea9468f2114931cb78ca6
Jemina M. <jemiruth1@gmail.com>
2025-10-15 20:10:19 -0500

    implemented to add players, process the queuem and determine the winner.

--- src/main/java/org/example/SingleEliminate.java
+++ src/main/java/org/example/SingleEliminate.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  */
 public class SingleEliminate implements TournamentInterface {
 
-    public List<String> queue = new ArrayList<>();
+    public List<Player> queue = new ArrayList<>();
 
     private Scanner scanner = new Scanner(System.in);
 
@@ -18,23 +18,38 @@ public class SingleEliminate implements TournamentInterface {
 
     @Override
     public void processQueue(List<Player> matches) {
+        List<Player> tempQueue = new ArrayList<>(matches);
+        for(int i = 0; i < tempQueue.size(); i+=2){
+            determineWinner(tempQueue.get(i), tempQueue.get(i+1));
+        }
 
     }
 
     @Override
     public void addPlayers(ArrayList<String> player) {
         for(String p : player){
-            queue.add(p);
+            queue.add(new Player(p));
         }
     }
 
     @Override
     public void determineWinner(Player playerOne, Player playerTwo) {
+        System.out.println("Who won?" + playerOne.getPlayerName() + " or " + playerTwo.getPlayerName());
+        String ans = scanner.nextLine().trim();
+        //if answer matches playerOne name, remove Player two from queue
+        // queue.remove(playerTwo);
+        //} else{
+        //    queue.remove(playerOne);
+        //}
 
+        queue.remove(ans.equals(playerOne.getPlayerName()) ? playerTwo : playerOne);
     }
 
     @Override
     public void runGame() {
-
+        while(queue.size() > 1){
+            processQueue(queue);
+        }
+        System.out.println("Winner is " + queue.get(0).getPlayerName());
     }
 }
